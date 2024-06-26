026183052a99e002769ac88fcc0d770e679c088e
Grish1221 <grishmita151@gmail.com>
2024-06-26 11:36:11 +0000

    done

--- basics/functions.js
+++ basics/functions.js
@@ -18,4 +18,45 @@ function calCartPrice(val1, val2, ...num1){
     return num1;
 }
 
-console.log(calCartPrice(1 ,34));
+// console.log(calCartPrice(1 ,34));
+
+const user={
+   name: "grish",
+   age: 19,
+   message: function(){
+       console.log(`Hello, ${this.name}`);
+       console.log(this);
+   }
+}
+
+// user.message();
+// user.name="naitik";
+// user.message();
+// console.log(this);
+
+// var my_func = function(){
+//     name: "grish",
+//     console.log(this);
+// }
+
+// my_func();
+
+// var my_func1 = () => {
+//     name: "grish",
+//     console.log(this);
+// }
+
+// my_func1();
+
+const arrowFunc = (num1, num2) => {
+    return num1+num2;
+}
+console.log(arrowFunc(2,3));
+
+const arrowFunc2 = (num1, num2) => (num1+num2);
+
+console.log(arrowFunc2(3,3));
+
+const arrowFunc3 = () => ({name: "Naitik"});
+
+console.log(arrowFunc3());
